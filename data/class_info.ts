c4b75ba47a833df8f07bb85f5d9f82477424ceeb
Apostolos Siokas <apostolossiokas@gmail.com>
2021-06-24 14:40:55 +0300

    test

--- data/class_info.ts
+++ data/class_info.ts
@@ -1,101 +1,106 @@
 import {
-    faUser,
-    faHistory,
-    faClock,
-    faEuroSign,
+  faUser,
+  faHistory,
+  faClock,
+  faEuroSign,
+  faCertificate,
 } from "@fortawesome/free-solid-svg-icons";
 
 export const infoDistance = [
-    {
-        title: "5 / τμήμα",
-        icon: faUser,
-    },
-    {
-        title: "30 εβδομάδες / έτος",
-        icon: faHistory,
-    },
-    {
-        title: "90 λεπά / εβδομάδα",
-        icon: faClock,
-    },
-    {
-        title: "300 μαζί με τον εξοπλισμό",
-        icon: faEuroSign,
-    },
+  {
+    title: "5 / τμήμα",
+    icon: faUser,
+  },
+  {
+    title: "30 εβδομάδες / έτος",
+    icon: faHistory,
+  },
+  {
+    title: "90 λεπά / εβδομάδα",
+    icon: faClock,
+  },
+  {
+    title: "300 μαζί με τον εξοπλισμό",
+    icon: faEuroSign,
+  },
 ];
 
 export const info3D = [
-    {
-        title: "8 / τμήμα",
-        icon: faUser,
-    },
-    {
-        title: "30 εβδομάδες / έτος",
-        icon: faHistory,
-    },
-    {
-        title: "90 λεπά / εβδομάδα",
-        icon: faClock,
-    },
-    {
-        title: "40 / μήνα",
-        icon: faEuroSign,
-    },
+  {
+    title: "8 / τμήμα",
+    icon: faUser,
+  },
+  {
+    title: "30 εβδομάδες / έτος",
+    icon: faHistory,
+  },
+  {
+    title: "90 λεπά / εβδομάδα",
+    icon: faClock,
+  },
+  {
+    title: "40 / μήνα",
+    icon: faEuroSign,
+  },
+  {
+    title: "Πιστοποίηση Παρακολούθησης",
+    icon: faCertificate,
+  },
 ];
 
 export const code = [
-    {
-        title: "8 / τμήμα",
-        icon: faUser,
-    },
-    {
-        title: "30 εβδομάδες / έτος",
-        icon: faHistory,
-    },
-    {
-        title: "90 λεπά / εβδομάδα",
-        icon: faClock,
-    },
-    {
-        title: "40 / μήνα",
-        icon: faEuroSign,
-    },
+  {
+    title: "8 / τμήμα",
+    icon: faUser,
+  },
+  {
+    title: "30 εβδομάδες / έτος",
+    icon: faHistory,
+  },
+  {
+    title: "90 λεπά / εβδομάδα",
+    icon: faClock,
+  },
+  {
+    title: "40 / μήνα",
+    icon: faEuroSign,
+  },
 ];
 
 export const arduino = [
-    {
-        title: "8 / τμήμα",
-        icon: faUser,
-    },
-    {
-        title: "30 εβδομάδες / έτος",
-        icon: faHistory,
-    },
-    {
-        title: "90 λεπά / εβδομάδα",
-        icon: faClock,
-    },
-    {
-        title: "40 / μήνα",
-        icon: faEuroSign,
-    },
+  {
+    title: "8 / τμήμα",
+    icon: faUser,
+  },
+  {
+    title: "30 εβδομάδες / έτος",
+    icon: faHistory,
+  },
+  {
+    title: "90 λεπά / εβδομάδα",
+    icon: faClock,
+  },
+  {
+    title: "40 / μήνα",
+    icon: faEuroSign,
+  },
 ];
 
 export const robotics = [
-    {
-        title: "8 / τμήμα",
-        icon: faUser,
-    },
-    {
-        title: "30 εβδομάδες / έτος",
-        icon: faHistory,
-    },
-    {
-        title: "90 λεπά / εβδομάδα",
-        icon: faClock,
-    },
-    {
-        title: "40 / μήνα",
-        icon: faEuroSign,
-    },
+  {
+    title: "8 / τμήμα",
+    icon: faUser,
+  },
+  {
+    title: "30 εβδομάδες / έτος",
+    icon: faHistory,
+  },
+  {
+    title: "90 λεπά / εβδομάδα",
+    icon: faClock,
+  },
+  {
+    title: "40 / μήνα",
+    icon: faEuroSign,
+  },
 ];
